95c6a7664a64fea604cedccdfc247f9e069b0738
p2717741 <p2717741@my365.dmu.ac.uk>
2024-05-03 12:24:40 +0100

    Added testing for Shipping class

---
+++ Testing5/tstShipping.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstShipping
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //Create an instance of the class we want to create
+            clsShipping AShipment = new clsShipping();
+            //Test to see that it exists
+            Assert.IsNotNull(AShipment);
+        }
+    }
+}
