de2feab97c87fb6bbc1a38b18c7fc04315f84fbd
Evans <p2720247@my365.dmu.ac.uk>
2024-05-07 13:01:45 +0100

    All test run

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -1,7 +1,15 @@
-﻿using System.Dynamic;
+﻿using System;
 
 namespace ClassLibrary
+{
     public class clsOrder
     {
+        public int orderId {  get; set; }
+        public int totalItems { get; set; }
+        public double totalPrice { get; set; }
+        public string address { get; set; }
+        public DateTime orderDate { get; set; }
+        public bool isAGift { get; set; }
+        public string itemCodes { get; set; }   
     }
 }
--- Testing2/tstOrder.cs
+++ Testing2/tstOrder.cs
@@ -15,5 +15,91 @@ namespace Testing2
             Assert.IsNotNull(AnOrder);
 
         }
+        [TestMethod]
+        public void orderIdPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            int TestData = 12;
+            //assign the data to the proerty
+            AnOrder.orderId = TestData;
+            //test to see that the values are the same
+            Assert.AreEqual(AnOrder.orderId, TestData);
+        }
+        [TestMethod]
+        public void totalItemsPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            int TestData = 1;
+            //assign the data to the proerty
+            AnOrder.totalItems = TestData;
+            //test to see that the values are the same
+            Assert.AreEqual(AnOrder.totalItems, TestData);
+        }
+        [TestMethod]
+        public void totalPriceOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            double TestData = 29.9900;
+            //assign the data to the proerty
+            AnOrder.totalPrice = TestData;
+            //test to see that the values are the same
+            Assert.AreEqual(AnOrder.totalPrice, TestData);
+        }
+
+        [TestMethod]
+        public void addressOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            string TestData = "9836 Main Street- North London-N76 9TK";
+            //assign the data to the proerty
+            AnOrder.address = TestData;
+            //test to see that the values are the same
+            Assert.AreEqual(AnOrder.address, TestData);
+        }
+
+        [TestMethod]
+        public void orderDatePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the proerty
+            AnOrder.orderDate = TestData;
+            //test to see that the values are the same
+            Assert.AreEqual(AnOrder.orderDate, TestData);
+        }
+        [TestMethod]
+        public void isAGiftPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            bool TestData = false;
+            //assign the data to the proerty
+            AnOrder.isAGift = TestData;
+            //test to see that the values are the same
+            Assert.AreEqual(AnOrder.isAGift, TestData);
+        }
+        public void itemCodes()
+        {
+            //create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            //create some test data to assign to the property
+            string TestData = "453-888-621";
+            //assign the data to the proerty
+            AnOrder.itemCodes = TestData;
+            //test to see that the values are the same
+            Assert.AreEqual(AnOrder.itemCodes, TestData);
+        }
     }
+    
 }
