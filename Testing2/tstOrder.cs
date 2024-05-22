cc1abc61790858c7b9138b1f21d6028fdd2f6abb
Evans <p2720247@my365.dmu.ac.uk>
2024-05-22 14:38:18 +0100

    Prac12 up to date

--- Testing2/tstOrder.cs
+++ Testing2/tstOrder.cs
@@ -8,7 +8,6 @@ namespace Testing2
     [TestClass]
     public class tstOrder
     {
-        [TestMethod]
         public void TestMethod1()
         {
             //create an instance of the class we want to create
